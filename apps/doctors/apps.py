a830c9bf3b3e933da7637fbf222ef599230a7a5d
Alokik Garg <alokikgarg24@gmail.com>
2026-09-04 22:38:21 +0530

    Add doctor model

--- apps/doctors/apps.py
+++ apps/doctors/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class DoctorsConfig(AppConfig):
-    name = 'doctors'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.doctors"
--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    specialization = models.CharField(max_length=150)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} - {self.specialization}"
